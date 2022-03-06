ffb7ce9d18a7840cb440ee592a713542c9761fb8
xuan giang <ngxuangiang2020@gmail.com>
2022-03-06 09:30:53 +0700

    First commit

---
+++ src/Phan2_Array_And_String/Bai1_Duyet_Mang.java
@@ -0,0 +1,10 @@
+package Phan2_Array_And_String;
+
+public class Bai1_Duyet_Mang {
+    public static void main(String[] args) {
+        // Khai báo
+        int[] a = {0,1,2,3,4,5};
+
+        int[] b = new int[5];
+    }
+}
